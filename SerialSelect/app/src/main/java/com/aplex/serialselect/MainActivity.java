ab58247aa08f5da9b9795df3a57a6a395f7e3966
maogefff <806593449@qq.com>
2017-12-13 16:17:58 +0800

    首次添加JNI

--- SerialSelect/app/src/main/java/com/aplex/serialselect/MainActivity.java
+++ SerialSelect/app/src/main/java/com/aplex/serialselect/MainActivity.java
@@ -2,12 +2,13 @@ package com.aplex.serialselect;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.aplex.serialselect.R;
 import com.aplex.serialselect.utils.SPUtils;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity  {
+//    Button bt;
     //static Application mApplication = MainActivity.getApplication();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +17,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         //SPUtils.testSPUtils(getApplication());
         //getApplication()
+//        bt.findViewById(R.id.Button1);
+//        bt.setOnClickListener();
     }
 }
